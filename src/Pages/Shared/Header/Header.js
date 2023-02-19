1fd8b191520f3708a8ed8dad9bf30aa8e26c708b
Md.Al kafee <sojeebahmed.sa@gmail.com>
2023-02-19 13:04:26 +0600

    navbar added

--- src/Pages/Shared/Header/Header.js
+++ src/Pages/Shared/Header/Header.js
@@ -1,40 +1,44 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
+import React, { useState } from 'react'
+import Button from './Button';
+import logo from "../../../assets/logo.png"
 
-const Header = () => {
 
-    const navItems = <React.Fragment>
-        <li><Link to='/'>Home</Link></li>
-        <li><Link to='/appointment'>Appointment</Link></li>
-        <li><Link to='/about'>About</Link></li>
-        <li><Link to='/reviews'>Reviews</Link></li>
-        <li><Link to='/login'>Login</Link></li>
+const Nav = () => {
+    let Links =[
+      {name:"HOME",link:"/"},
+      {name:"SERVICE",link:"/"},
+      {name:"ABOUT",link:"/"},
+      {name:"BLOG'S",link:"/"},
+      {name:"CONTACT",link:"/"},
+    ];
+    let [open,setOpen]=useState(false);
+  return (
+    <div className='shadow-md w-full fixed top-0 left-0'>
+      <div className='md:flex items-center justify-between bg-white py-4 md:px-10 px-7'>
+      <div className='font-bold text-2xl cursor-pointer flex items-center font-[Poppins] 
+      text-gray-800'>
+        <img className='w-52' src={logo} alt="" srcset="" />
+      </div>
+      
+      <div onClick={()=>setOpen(!open)} className='text-3xl absolute right-8 top-6 cursor-pointer md:hidden'>
+      <ion-icon name={open ? 'close':'menu'}></ion-icon>
+      </div>
 
-    </React.Fragment>
-
-    return (
-        <div className="navbar bg-base-100 flex justify-between">
-        <div className="navbar-start">
-            <div className="dropdown">
-                <label tabIndex={0} className="btn btn-ghost lg:hidden">
-                    <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" /></svg>
-                </label>
-                <ul tabIndex={1} className="menu menu-compact dropdown-content mt-3 p-2 shadow bg-base-100 rounded-box w-52">
-                    {navItems}
-                </ul>
-            </div>
-            <Link to='/' className="btn btn-ghost normal-case text-xl">Doctor's Portal</Link>
-        </div>
-        <div className="navbar-center hidden lg:flex">
-            <ul className="menu menu-horizontal p-0">
-                {navItems}
-            </ul>
-        </div>
-        {/* <label htmlFor="dashboard-drawer" tabIndex={2} className="btn btn-ghost lg:hidden">
-            <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" /></svg>
-        </label> */}
+      <ul className={`md:flex md:items-center md:pb-0 pb-12 absolute md:static bg-white md:z-auto z-[-1] left-0 w-full md:w-auto md:pl-0 pl-9 transition-all duration-500 ease-in ${open ? 'top-20 ':'top-[-490px]'}`}>
+        {
+          Links.map((link)=>(
+            <li key={link.name} className='md:ml-8 text-sm md:my-0 my-7'>
+              <a href={link.link} className='text-gray-800 hover:text-gray-400 duration-500'>{link.name}</a>
+            </li>
+          ))
+        }
+        {/* <Button>
+          Get Started
+        </Button> */}
+      </ul>
+      </div>
     </div>
-    );
-};
+  )
+}
 
-export default Header;
+export default Nav
