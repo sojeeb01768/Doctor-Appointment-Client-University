ac6d5f87f0b214f7ad7a9d7433bec8f26c7ab75e
Mujahidul Amin <mujahidulamin1108152@gmail.com>
2023-03-20 01:25:45 +0600

    space

--- src/Pages/Consultation/Consultation.js
+++ src/Pages/Consultation/Consultation.js
@@ -30,6 +30,10 @@ const Consultation = () => {
                         speciality={speciality}
                     ></Specialities>
                 )
+
+
+
+                
             }
         </div>
     );
