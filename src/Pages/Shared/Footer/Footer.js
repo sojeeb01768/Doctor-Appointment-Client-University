5e86dcb2fb5fbc8cec098d0fee25d9db2716c7bc
Mujahidul Amin <mujahidulamin1108152@gmail.com>
2023-03-19 10:45:51 +0600

    foter

--- src/Pages/Shared/Footer/Footer.js
+++ src/Pages/Shared/Footer/Footer.js
@@ -1,45 +1,133 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
-// import footer from '../../../assets/images/footer.png'
+import React from "react";
+import "./Footes.css";
+import { FaMapMarkerAlt } from "react-icons/fa";
+import { FaPhoneAlt } from "react-icons/fa";
+import { FaWarehouse } from "react-icons/fa";
+import { FaLinkedin } from "react-icons/fa";
+import { FaTwitter } from "react-icons/fa";
+import { FaInstagram } from "react-icons/fa";
+import { FaFacebookF } from "react-icons/fa";
+import { Link } from "react-router-dom";
 
+//Footer section
 const Footer = () => {
-    return (
-        <footer
-            // style={{
-            //     background: `url(${footer})`,
-            //     backgroundSize: 'cover'
-            // }}
-            className="p-10"
-        >
+  return (
+    <div className="bg-footer pb-14 text-white">
+      <div className="hero ">
+        <div className="hero-content2 lg:gap-36 flex-col lg:flex-row mt-8">
+          <div className="lg:text-left">
+            <div className="mt-12">
+              <p className="title-contact">WE'D LOVE TO HERE FROM YOU</p>
 
+              <div className="flex items-center contact mt-10">
+                <FaWarehouse className="mr-6"></FaWarehouse>
+                <span>contact@erminautomotive.com</span>
+              </div>
 
-            <div className="footer">
-                <div className='mx-auto'>
-                    <span className="footer-title">Services</span>
-                    <Link to='/' className="link link-hover">Branding</Link>
-                    <Link to='/' className="link link-hover">Design</Link>
-                    <Link to='/' className="link link-hover">Marketing</Link>
-                    <Link to='/' className="link link-hover">Advertisement</Link>
+              <div className="flex items-center mt-5 contact">
+                <FaMapMarkerAlt className="mr-6"></FaMapMarkerAlt>
+                <span>
+                  Plot no: 68 TNGOs Gachibowli, <br />
+                  Hyderabad, Telangana 500032
+                </span>
+              </div>
+
+              <div className="flex items-center contact mt-5">
+                <FaPhoneAlt className="mr-6"></FaPhoneAlt>
+                <span>040-46028449</span>
+              </div>
+
+              <div>
+                <h1 className="title-contact mt-24 mb-5">FOLLOW US</h1>
+                <div className="flex">
+                  <a
+                    href="https://www.linkedin.com/company/ermin-automotive/mycompany/"
+                    className="mr-8"
+                    target="blank"
+                  >
+                    <FaLinkedin></FaLinkedin>
+                  </a>
+                  <a
+                    href="https://www.facebook.com/profile.php?id=100066954733805"
+                    className="mr-8"
+                    target="blank"
+                  >
+                    <FaFacebookF></FaFacebookF>
+                  </a>
+                  <a
+                    href="https://www.instagram.com/erminautomotive/"
+                    className="mr-8"
+                    target="blank"
+                  >
+                    <FaInstagram></FaInstagram>
+                  </a>
+                  <a
+                    href="https://twitter.com/ErminAutomotive"
+                    className="mr-8"
+                    target="blank"
+                  >
+                    <FaTwitter></FaTwitter>
+                  </a>
                 </div>
-                <div className='mx-auto'>
-                    <span className="footer-title">Company</span>
-                    <Link to='/' className="link link-hover">About us</Link>
-                    <Link to='/' className="link link-hover">Contact</Link>
-                    <Link to='/' className="link link-hover">Jobs</Link>
-                    <Link to='/' className="link link-hover">Press kit</Link>
-                </div>
-                <div className='mx-auto'>
-                    <span className="footer-title">Legal</span>
-                    <Link to='/' className="link link-hover">Terms of use</Link>
-                    <Link to='/' className="link link-hover">Privacy policy</Link>
-                    <Link to='/' className="link link-hover">Cookie policy</Link>
-                </div>
+              </div>
             </div>
-            <div className='text-center mt-20'>
-                <p >Copyright © 2023 - All right reserved by  Mujahidul amin & Md. Al kafee</p>
-            </div>
-        </footer>
-    );
+          </div>
+
+          <div>
+            <h1 className="mt-12 title-contact mb-10">QUICK CONTACT</h1>
+            <form>
+              <div className="flex justify-between">
+                <input
+                  type="text"
+                  className="input-bg w-5/12"
+                  placeholder="Full Name"
+                  required
+                />
+
+                <input
+                  type="email"
+                  className="input-bg w-5/12"
+                  placeholder="Email"
+                  required
+                />
+              </div>
+
+              <div>
+                <input
+                  type="text"
+                  className="input-bg mt-8 w-full"
+                  placeholder="Phone Number"
+                  required
+                />
+              </div>
+              <div>
+                <input
+                  name="message"
+                  className="input-bg mt-10 w-full mb-10"
+                  placeholder="Message"
+                  required
+                />
+              </div>
+
+              <div className="form-control mt-4">
+                <input
+                  className="btn btn-outline btn-success"
+                  type="submit"
+                  value="SUBMIT"
+                  required
+                />
+              </div>
+              <div className="mt-8 text-gray-400">
+                <small className="text-inherit">
+                  @ Copyright © 2021 Ermin Automotive. All rights reserved
+                </small>
+              </div>
+            </form>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Footer;
