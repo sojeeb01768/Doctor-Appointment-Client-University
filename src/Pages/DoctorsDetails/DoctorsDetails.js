b2532fae647b88f6911737389884e968cd85ade5
Md.Al kafee <sojeebahmed.sa@gmail.com>
2023-04-09 14:48:48 +0600

    test

--- src/Pages/DoctorsDetails/DoctorsDetails.js
+++ src/Pages/DoctorsDetails/DoctorsDetails.js
@@ -4,10 +4,10 @@ import { useLoaderData } from 'react-router-dom';
 const DoctorsDetails = () => {
     const doctor = useLoaderData();
     console.log(doctor);
-    
+
     return (
-        <div className='mt-20'>
-            <h2>This is details</h2>
+        <div className='mt-20 mx-auto text-center text-4xl'>
+            <h2>{doctor?.name}</h2>
         </div>
     );
 };
